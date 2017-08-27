7675b67f6f7694e0c723595e4f624db03dd2d706
Rafael Wolf de Goes <rewgoes@gmail.com>
2017-08-27 18:52:01 -0300

    Closes #5: implement UI test

---
+++ app/src/androidTest/java/com/wolfgoes/bakingapp/RecipeListActivityScreenTest.java
@@ -0,0 +1,56 @@
+package com.wolfgoes.bakingapp;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.wolfgoes.bakingapp.ui.RecipeListActivity;
+import com.wolfgoes.bakingapp.util.Constants;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeListActivityScreenTest {
+
+    @Rule
+    public IntentsTestRule<RecipeListActivity> mActivityRule = new IntentsTestRule<>(
+            RecipeListActivity.class);
+
+
+    private IdlingResource mIdlingResource;
+
+    // Registers any resource that needs to be synchronized with Espresso before the test is run.
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
+    @Test
+    public void clickGridViewItem_OpensStepsActivity() throws Exception {
+        onView(allOf(withId(R.id.recipe_name), withText("Nutella Pie"))).perform(click());
+
+        intended(hasExtraWithKey(Constants.EXTRA_RECIPE));
+    }
+
+    // Unregister resources when not needed to avoid malfunction.
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
+    }
+}
--- app/src/main/java/com/wolfgoes/bakingapp/ui/RecipeListActivity.java
+++ app/src/main/java/com/wolfgoes/bakingapp/ui/RecipeListActivity.java
@@ -5,13 +5,18 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.wolfgoes.bakingapp.IdlingResource.SimpleIdlingResource;
 import com.wolfgoes.bakingapp.R;
 import com.wolfgoes.bakingapp.adapter.RecipeAdapter;
 import com.wolfgoes.bakingapp.model.Recipe;
@@ -44,11 +49,24 @@ public class RecipeListActivity extends AppCompatActivity implements Callback<Ar
     @BindView(R.id.message_view)
     TextView mMessageView;
 
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putParcelableArrayList(Constants.STATE_EXTRA_RECIPES_ARRAY, mRecipes);
         outState.putBoolean(Constants.STATE_EXTRA_CHOOSE_RECIPE, mIsChooseRecipe);
         super.onSaveInstanceState(outState);
+    }
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+            mIdlingResource.setIdleState(false);
+        }
+        return mIdlingResource;
     }
 
     @Override
@@ -79,6 +97,11 @@ public class RecipeListActivity extends AppCompatActivity implements Callback<Ar
 
             Call<ArrayList<Recipe>> call = recipeApi.loadRecipes();
             mMessageView.setText(getString(R.string.loading_recipes));
+
+            if (mIdlingResource != null) {
+                mIdlingResource.setIdleState(false);
+            }
+
             call.enqueue(this);
         } else {
             mRecyclerView.setVisibility(View.VISIBLE);
@@ -104,6 +127,8 @@ public class RecipeListActivity extends AppCompatActivity implements Callback<Ar
             mRecyclerView.setVisibility(View.GONE);
             mMessageView.setVisibility(View.VISIBLE);
         }
+
+        setIdleState();
     }
 
     @Override
@@ -111,6 +136,20 @@ public class RecipeListActivity extends AppCompatActivity implements Callback<Ar
         mMessageView.setText(getString(R.string.error_loading_recipes));
         mRecyclerView.setVisibility(View.GONE);
         mMessageView.setVisibility(View.VISIBLE);
+
+        setIdleState();
+    }
+
+    private void setIdleState() {
+        if (mIdlingResource != null) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIdlingResource.setIdleState(true);
+                }
+            }, 1000);
+        }
     }
 
     @Override
